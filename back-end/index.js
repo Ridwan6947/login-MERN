cfe0ddd646b3371ab10c2495f4c3a066c482c782
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2024-02-20 16:15:49 +0530

    new updates

--- back-end/index.js
+++ back-end/index.js
@@ -4,7 +4,14 @@ const cors = require('cors')
 const Login = require('./models/login');
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin : ["https://login-dl56cyxsx-ridwan6947s-projects.vercel.app"],
+        methods : [GET , POST],
+        credentials : true
+
+    }
+));
 app.use(express.json());
 
 const mongoDBURL = 'mongodb+srv://root:root@cluster0.eyccxmh.mongodb.net/?retryWrites=true&w=majority';
