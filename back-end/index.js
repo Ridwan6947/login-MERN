f8589d7a25ddbfe500e00f64d440ab6bc3a589fc
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2024-02-21 18:13:11 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,53 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
+const { GET, POST } = require('express'); // Import GET and POST methods from Express
 const Login = require('./models/login');
 
 const app = express();
-app.use(cors(
-    {
-        origin : ["https://login-mern-api-lake.vercel.app"],
-        methods : [GET , POST],
-        credentials : true
-
-    }
-));
+app.use(cors({
+    origin: ["https://login-mern-api-lake.vercel.app"],
+    methods: ['GET', 'POST'], // Specify HTTP methods as strings
+    credentials: true
+}));
 app.use(express.json());
 
 const mongoDBURL = 'mongodb+srv://root:root@cluster0.eyccxmh.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(mongoDBURL)
-    .then(() =>{
+    .then(() => {
         console.log("DataBase Connected");
-        app.listen(5000 , ()=>{
+        app.listen(5000, () => {
             console.log("server is running");
-        })
+        });
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log(err);
     });
 
+app.post('/', async (req, res) => {
+    res.json("Hello");
+});
 
-    app.post('/' , async(req , res) =>{
-        res.json("Hello")
-    });
-    app.post('/submit' , async(req , res) =>{
-        const {email,password} = req.body;
-        Login.findOne({email : email})
-        .then(user =>{
-            if(user){
-                if(user.password === password){
+app.post('/submit', async (req, res) => {
+    const { email, password } = req.body;
+    Login.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
                     res.json("Login Successfull");
-                    return res.status(200).send({message :'Logged in Successfully'})
+                } else {
+                    res.json("Login failed , Check password again");
                 }
-                else{
-                    res.json("Login failed , Check password again")
-                }
-            }else{
-                res.json("No record found , create a new account")
+            } else {
+                res.json("No record found , create a new account");
             }
-        })
-    });
+        });
+});
 
     app.post('/newUser' , async(req , res) =>{
         try{
